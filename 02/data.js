a74dea86946dceea191a9aff4e27ccef9e24a1b3
Albin Hallden <albin.hallden@cybercom.com>
2015-12-09 08:50:56 +0100

    Updated solution

--- 02/data.js
+++ 02/data.js
@@ -1,27 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-(() => {
-    const data = fs.readFileSync(path.join(process.cwd(), 'data.txt')).toString();
-    const dataList = data.split('\r\n');
-    var sum = 0;
+const data = fs.readFileSync(path.join(process.cwd(), 'data2.txt')).toString();
+const dataList = data.split('\r\n');
+var sum = 0;
 
-    // 2*l*w + 2*w*h + 2*h*l
-    for (var i = 0; i < dataList.length; i++) {
-        var current = dataList[i].split('x');
+// 2*l*w + 2*w*h + 2*h*l
+for (var i = 0; i < dataList.length; i++) {
+    var value = dataList[i].split('x');
 
-        var l = parseInt(current[0], 10);
-        var w = parseInt(current[1], 10);
-        var h = parseInt(current[2], 10);
+    var l = parseInt(value[0], 10);
+    var w = parseInt(value[1], 10);
+    var h = parseInt(value[2], 10);
 
-        var a1 = l*w;
-        var a2 = w*h;
-        var a3 = h*l;
+    var a1 = l*w;
+    var a2 = w*h; 
+    var a3 = h*l;
 
-        var min = Math.min(a1, a2, a3);
-
-        sum += (2*a1) + (2*a2) + (2*a3) + min;
-    };
-    console.log(sum);
-
-})()
+    sum += (2*a1) + (2*a2) + (2*a3) + Math.min(a1, a2, a3);
+};
+console.log(sum);
