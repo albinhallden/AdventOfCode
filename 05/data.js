1566a556350d9f4f76ec35f7f1d3b8bc4cabce8e
Albin Hallden <albin.hallden@cybercom.com>
2015-12-10 16:44:03 +0100

    Updated solution for problem 05

--- 05/data.js
+++ 05/data.js
@@ -2,17 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const data = fs.readFileSync(path.join(process.cwd(), 'data.txt')).toString();
 const dataList = data.split('\r\n');
-var correct = 0;
 
-for (var i = 0; i < dataList.length; i++) {
-  var input = dataList[i];
+function myFilter(input) {
   var vowels = input.match(/[aeiou]/g);
   var duplicate = input.match(/([a-z])\1/);
   var invalid = input.match(/ab|cd|pq|xy/);
-
-  if (invalid === null && vowels !== null && vowels.length > 2 && duplicate !== null) {
-    correct++;
-  }
+  return (invalid === null && vowels !== null && vowels.length > 2 && duplicate !== null);
 }
-
-console.log(correct);
+console.log(dataList.filter(myFilter).length);
