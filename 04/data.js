15bfb411fb912e7b92250cacdd808e707f3e3c96
Albin Hallden <albin.hallden@cybercom.com>
2015-12-16 08:46:10 +0100

    Correct mistake of adding code by mistake

--- 04/data.js
+++ 04/data.js
@@ -1,22 +1,11 @@
 var md5 = require('md5');
-var key = 'ckczppom';
+var key = 'iwrupvqb';
 
-var MAX = 1000000; // Could be raised if no answer is found within limit
+var MAX = 1000000;
 for (var i = 1; i < MAX; i++) {
   var hash = md5(key + i);
   if (hash.match('^0{5}')) {
     console.log('a:', i);
-    break;
-  }
-}
-
-var input = key;
-
-for (let i = 1; i < 1000000; ++i) {
-  const match = md5(input + i).startsWith('00000');
-
-  if (match) {
-    console.log('j:', i);
     break;
   }
 }
